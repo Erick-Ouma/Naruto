16776e11e2a787a544c3f8c793c5faafe8a27225
Mohamed <117613748+mohalicious@users.noreply.github.com>
2023-07-03 00:01:59 +0300

    naruto

--- plugins/main-Guru.js
+++ plugins/main-Guru.js
@@ -1,33 +1,33 @@
 let handler = async m => m.reply(`
 
-≡  *GURU  ┃ ᴮᴼᵀ*   GROUPS
+≡  *NARUTO  ┃ ᴮᴼᵀ*   GROUPS
 
 ─────────────
 ▢ Join public bot group and support
-https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
+https://chat.whatsapp.com/BwNJduDcXijJAWkTVJol9h
 
 ▢ Group 2
-https://chat.whatsapp.com/LwLQab5mZVW6WWRbBPnDGC
+https://chat.whatsapp.com/HGUBy2g9nlWGVnCgYDuP2j
 
 ▢ Group 3
-https://chat.whatsapp.com/Jn9qVerYN6Q1L0vlVFe2XL
+https://chat.whatsapp.com/BwNJduDcXijJAWkTVJol9h
 
 ─────────────
 ≡ Disabled links? enter here! 
 
 ▢ Group WhatsApp 
- https://chat.whatsapp.com/F3sB3pR3tClBvVmlIkqDJp
+ https://chat.whatsapp.com/HGUBy2g9nlWGVnCgYDuP2j
 ─────────────
 ▢ *Owner Telegram*
- https://t.me/i_want_to_be_isekaied
+ punstic
 
 ▢ *YouTube*
-• https://www.youtube.com/@Asliguru
+• https://www.youtube.com/@donbel_ovibel
 
 
 `.trim())
-handler.help = ['gpguru']
+handler.help = ['gpnaruto']
 handler.tags = ['main']
-handler.command = ['groups', 'groupguru', 'gugp', 'ggp', 'gpguru'] 
+handler.command = ['groups', 'groupnaruto', 'gugp', 'ggp', 'gpguru'] 
 
 export default handler
